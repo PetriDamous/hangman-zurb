66e63da361713b1ddd6e7f215648b2ed7f825b64
dwilliams64 <dwilliams_64@yahoo.com>
2019-11-22 16:38:28 -0600

    Added keyboard controls for game

--- src/assets/js/app/hangman.js
+++ src/assets/js/app/hangman.js
@@ -1,16 +1,3 @@
-// Challenge 1:
-// 1. Create a constructor function for the hangman game
-// 2. Setup two attributes. One for the word itself. Another for the number of guesses allowed.
-// 3. Create two instances of it and print both to the console
-
-// Challenge 2:
-// 1. Set up the word instance property as an array of lower case letters
-// 2. Set up another instance property to store guessed letters
-// 3. Create a method that gives you the word puzzle back
-
-// No guesses? -> ***
-// Guessed "d", "b", and "g"? -> d*g
-
 const Hangman = function(word, tries) {
     this.word = word.toLowerCase().split('');
     this.tries = tries;    
@@ -48,24 +35,19 @@ Hangman.prototype.getGuess = function (guess) {
 }
 
 const game1 = new Hangman('Dog', 2);
-// Guess d, g, f
-game1.getGuess('d');
-game1.getGuess('g');
-game1.getGuess('f');
 
-game1.getGuess('d');
-game1.getGuess('g');
-game1.getGuess('f');
 
-console.log(game1.getPuzzle()); // d*g
+console.log(game1.getPuzzle());
 
-// Print remaing guesses (should be 1)
 console.log(game1.tries);
 
-const game2 = new Hangman('New Jersey', 4);
+window.addEventListener('keypress', function(e) {
+    const guess = e.key;
 
-const game3 = new Hangman('Donkey Kong', 4);
+    game1.getGuess(guess);
 
+    console.log(game1.getPuzzle());
+    console.log(game1.tries);
 
-// console.log(game2.getPuzzle());
-// console.log(game3.getPuzzle());
+});
+
