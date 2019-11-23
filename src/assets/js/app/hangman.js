7ee3ef719dd0d9b1734dda2266dbfdb87a5cbb9c
dwilliams64 <dwilliams_64@yahoo.com>
2019-11-23 15:19:42 -0600

    Added DOM rendering and game status tracking

--- src/assets/js/app/hangman.js
+++ src/assets/js/app/hangman.js
@@ -2,6 +2,7 @@ const Hangman = function(word, tries) {
     this.word = word.toLowerCase().split('');
     this.tries = tries;    
     this.guessLetters = [];
+    this.status = 'playing';
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -34,20 +35,54 @@ Hangman.prototype.getGuess = function (guess) {
     }
 }
 
+Hangman.prototype.gameStatus = function () {
+
+    const finish = this.word.every( (letter) => this.guessLetters.includes(letter) );
+
+    // Changes status    
+    if (finish) {
+        this.status = 'finished'
+        console.log(this.status)
+    } else if (this.tries <= 0) {
+        this.status = 'failed'
+        console.log(this.status)
+    } else {
+        this.status = 'playing'
+        console.log(this.status)
+    }
+    
+}
+
+Hangman.prototype.renderPuzzle = function () {
+    // Puzzle area
+    const puzzleArea = document.getElementById('puzzle-area');
+
+    puzzleArea.innerHTML = '';
+
+    // Creates puzzle element
+    const displaypuzzle = document.createElement('div');
+    displaypuzzle.textContent = this.getPuzzle();
+    puzzleArea.appendChild(displaypuzzle);
+
+    // Creates guess counter
+    const displayGuess = document.createElement('div');
+    displayGuess.textContent = this.tries;
+    puzzleArea.appendChild(displayGuess);
+
+}
+
 const game1 = new Hangman('Dog', 2);
 
-
-console.log(game1.getPuzzle());
-
-console.log(game1.tries);
+game1.renderPuzzle();
 
 window.addEventListener('keypress', function(e) {
     const guess = e.key;
 
     game1.getGuess(guess);
 
-    console.log(game1.getPuzzle());
-    console.log(game1.tries);
+    game1.gameStatus();
+
+    game1.renderPuzzle();
 
 });
 
